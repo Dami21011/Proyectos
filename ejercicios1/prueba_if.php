5936afb676c249852bff5d6b282d7bd565d7ef3b
Dami21011 <damrodlop@alu.edu.gva.es>
2024-09-23 16:36:20 +0200

    Ejercicio de pureba if creado

---
+++ ejercicios1/prueba_if.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba if</title>
+</head>
+<body>
+    <?php
+    $nota1 = 6.5;
+    $nota2 = 2;
+    
+    if ($nota1 > $nota2) {
+        echo "La nota 1 es mayor";
+    } else {
+        echo "La nota 2 es mayor";
+    }
+
+    ?>
+</body>
+</html>
