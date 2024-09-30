9ccc21216590dc6fc0348a0bbc5f6cda7f055dfc
Dami21011 <damrodlop@alu.edu.gva.es>
2024-09-30 17:37:31 +0200

    Modificacion realizada en copia_datos.php

--- ejercicios2/copia_seguridad.php
+++ ejercicios2/copia_seguridad.php
@@ -3,18 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>COpia de seguridad</title>
 </head>
 <body>
     <?php
         $archivo_origen = 'datos.txt';
-
         $archivo_copia = 'copia_datos.txt';
+
+        $nuevo_email = 'damianmola45327@gmail.com';
+
+        $patron_email = '/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/';
 
         if (file_exists($archivo_origen)) {
             $contenido = file_get_contents($archivo_origen);
 
-            if (file_put_contents($archivo_copia, $contenido) !== false) {
+            $contenido_modificado = preg_replace($patron_email, $nuevo_email, $contenido);
+
+            if (file_put_contents($archivo_copia, $contenido_modificado) !== false) {
                 echo "Copia de seguridad creada correctamente en 'copia_datos.txt'.";
             } else {
                 echo "Error al crear la copia de seguridad.";
