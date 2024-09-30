861832001d74d2b69438fd9a3caba93090263dd1
Dami21011 <damrodlop@alu.edu.gva.es>
2024-09-30 16:34:08 +0200

    cambios

--- ejercicios2/ahoraAmpliacion.php
+++ ejercicios2/ahoraAmpliacion.php
@@ -12,22 +12,22 @@
 
         $fechaNac = '2003-06-21';
         $fechaActual = date('Y-m-d');
-
+        
         echo "Fecha de nacimiento: " . date('d/m/Y', strtotime($fechaNac)) . "<br>";
-
+        
         $partesFecha = explode('-', $fechaNac);
         echo "¿Fecha Válida? " . (checkdate($partesFecha[1], $partesFecha[2], $partesFecha[0]) ? "Sí" : "No") . "<br>";
-
+        
         $segundos = strtotime($fechaActual) - strtotime($fechaNac);
-
+        
         $años = floor($segundos / (365 * 24 * 60 * 60));
         $segundos_restantes = $segundos - ($años * 365 * 24 * 60 * 60);
         $meses = floor($segundos_restantes / (30 * 24 * 60 * 60));
         $segundos_restantes -= $meses * (30 * 24 * 60 * 60);
         $días = floor($segundos_restantes / (24 * 60 * 60));
-
+        
         echo "Tienes " . $segundos . " segundos de vida, que equivalen a " . 
-             $años . " años, " . $meses . " meses y " . $días . " días.";
+                     $años . " años, " . $meses . " meses y " . $días . " días.";
     ?>
 </body>
 </html>
