9ff2e05f57b0811b3da27c841e03e86927eb91ec
Dami21011 <damrodlop@alu.edu.gva.es>
2024-10-02 16:13:39 +0200

    curriculum.php creado pero sin acabar

---
+++ ejercicios3/curriculum.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculum</title>
+</head>
+<body>
+    <h1>Mi Curriculum</h1>
+<?php 
+        $estudios_es = "Estudios cursados: GM SMR / GS DAW";
+        $idiomas_es = "Idiomas que hablo: Español / Inglés / Valenciano";
+        $estudios_en = "Studies completed: GM SMR / GS DAW";
+        $idiomas_en = "Languages I speak: Spanish / English / Valencian";
+        
+        $idioma = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        $estudios_var = "estudios_" . $idioma;
+        $idiomas_var = "idiomas_" . $idioma;
+        
+        echo @("<p>${$estudios_var}</p>");
+        echo @("<p>${$idiomas_var}</p>");
+    ?>
+</body>
+</html>
