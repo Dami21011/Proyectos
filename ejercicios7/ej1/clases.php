cd7238aeabd41c7ebce965c4b28b87e68243ec10
Dami21011 <damrodlop@alu.edu.gva.es>
2024-10-03 19:16:22 +0200

    Añadidos el antes y el despues para que se vean los setters en clases.php

--- ejercicios7/ej1/clases.php
+++ ejercicios7/ej1/clases.php
@@ -15,14 +15,20 @@
         $Persona1 = new Persona("26889321K","Damián Rodríguez López","damrodlop@alu.edu.gva.es");
 
         echo "<p><b><u>Persona</u></b></p>";
+        echo "<b>Antes: </b>";
+        $Persona1->Mostrar();
         $Persona1->__setNombre("Manuel Valero Gimeno");
         $Persona1->__setDNI("11223344L");
         $Persona1->__setEmail("manvalgim@alu.edu.gva.es");
+        echo "<b>Despues: </b>";
         $Persona1->Mostrar();
 
         echo "<p><b><u>Estudiante</u></b></p>";
         $Estudiante1 = new Estudiante("26889321K","Damián Rodríguez López","damrodlop@alu.edu.gva.es","10622101");
+        echo "<b>Antes: </b>";
+        $Estudiante1->Mostrar();
         $Estudiante1->__setnumExpediente("10689101");
+        echo "<b>Despues: </b>";
         $Estudiante1->Mostrar();
     ?>
 </body>
